28304db746e649b7f6a7d736bfbb1ad916e01d61
Guillaume JUSTE <guillaume.juste.pro@gmail.com>
2022-07-14 14:04:02 +0900

    [Add] render pass

---
+++ RenderEngine/Include/Engine/RenderPass/RenderPass.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef RENDERENGINE_RENDERPASS
+#define RENDERENGINE_RENDERPASS
+
+#include "Vulkan/VulkanBaseInclude.hpp"
+#include "Engine/RenderPass/RenderPassCreateInfo.hpp"
+
+namespace RenderEngine
+{
+	class RenderPass
+	{
+	private:
+		VkDevice* logicalDevice = VK_NULL_HANDLE;
+		VkRenderPass renderPass;
+
+	public:
+		RenderPass() = default;
+		~RenderPass() = default;
+
+		static void InitializeRenderPass(RenderPassCreateInfo _createInfo, RenderPass* _output);
+		void Cleanup();
+	};
+
+}
+
+#endif
---
+++ RenderEngine/Include/Engine/RenderPass/RenderPassCreateInfo.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef RENDERENGINE_RENDERPASSCREATEINFO
+#define RENDERENGINE_RENDERPASSCREATEINFO
+
+#include "Engine/Shader/Shader.hpp"
+
+namespace RenderEngine
+{
+	struct RenderPassCreateInfo
+	{
+		VkDevice* logicalDevice;
+		VkFormat swapChainImageFormat;
+
+		RenderPassCreateInfo() = default;
+	};
+}
+
+#endif
